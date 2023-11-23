cabd133141a4eedba833948ca074bf695c9fb7f0
alpakts <ultrafakto@gmail.com>
2023-11-23 14:42:57 +0300

    init

---
+++ script.js
@@ -0,0 +1,146 @@
+// VoiceRSS Javascript SDK
+var joke;
+const audioElement = document.querySelector("#audio");
+const VoiceRSS = {
+  speech: function (e) {
+    this._validate(e), this._request(e);
+  },
+  _validate: function (e) {
+    if (!e) throw "The settings are undefined";
+    if (!e.key) throw "The API key is undefined";
+    if (!e.src) throw "The text is undefined";
+    if (!e.hl) throw "The language is undefined";
+    if (e.c && "auto" != e.c.toLowerCase()) {
+      var a = !1;
+      switch (e.c.toLowerCase()) {
+        case "mp3":
+          a = new Audio().canPlayType("audio/mpeg").replace("no", "");
+          break;
+        case "wav":
+          a = new Audio().canPlayType("audio/wav").replace("no", "");
+          break;
+        case "aac":
+          a = new Audio().canPlayType("audio/aac").replace("no", "");
+          break;
+        case "ogg":
+          a = new Audio().canPlayType("audio/ogg").replace("no", "");
+          break;
+        case "caf":
+          a = new Audio().canPlayType("audio/x-caf").replace("no", "");
+      }
+      if (!a) throw "The browser does not support the audio codec " + e.c;
+    }
+  },
+  _request: function (e) {
+    var a = this._buildRequest(e),
+      t = this._getXHR();
+    (t.onreadystatechange = function () {
+      if (4 == t.readyState && 200 == t.status) {
+        if (0 == t.responseText.indexOf("ERROR")) throw t.responseText;
+        (audioElement.src = t.responseText), audioElement.play();
+      }
+    }),
+      t.open("POST", "https://api.voicerss.org/", !0),
+      t.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded; charset=UTF-8"
+      ),
+      t.send(a);
+  },
+  _buildRequest: function (e) {
+    var a = e.c && "auto" != e.c.toLowerCase() ? e.c : this._detectCodec();
+    return (
+      "key=" +
+      (e.key || "") +
+      "&src=" +
+      (e.src || "") +
+      "&hl=" +
+      (e.hl || "") +
+      "&r=" +
+      (e.r || "") +
+      "&c=" +
+      (a || "") +
+      "&f=" +
+      (e.f || "") +
+      "&ssml=" +
+      (e.ssml || "") +
+      "&b64=true"
+    );
+  },
+  _detectCodec: function () {
+    var e = new Audio();
+    return e.canPlayType("audio/mpeg").replace("no", "")
+      ? "mp3"
+      : e.canPlayType("audio/wav").replace("no", "")
+      ? "wav"
+      : e.canPlayType("audio/aac").replace("no", "")
+      ? "aac"
+      : e.canPlayType("audio/ogg").replace("no", "")
+      ? "ogg"
+      : e.canPlayType("audio/x-caf").replace("no", "")
+      ? "caf"
+      : "";
+  },
+  _getXHR: function () {
+    try {
+      return new XMLHttpRequest();
+    } catch (e) {}
+    try {
+      return new ActiveXObject("Msxml3.XMLHTTP");
+    } catch (e) {}
+    try {
+      return new ActiveXObject("Msxml2.XMLHTTP.6.0");
+    } catch (e) {}
+    try {
+      return new ActiveXObject("Msxml2.XMLHTTP.3.0");
+    } catch (e) {}
+    try {
+      return new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {}
+    try {
+      return new ActiveXObject("Microsoft.XMLHTTP");
+    } catch (e) {}
+    throw "The browser does not support HTTP request";
+  },
+};
+
+const getVoice = async (joke) => {
+  const encodedParams = new URLSearchParams();
+  encodedParams.set("src", "yarrağımı yedinn");
+  encodedParams.set("hl", "TR-tr");
+  encodedParams.set("r", "0");
+  encodedParams.set("c", "mp3");
+  encodedParams.set("f", "8khz_8bit_mono");
+
+  const url = "https://voicerss-text-to-speech.p.rapidapi.com/?key=";
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Key": "75cff8485fmsh3f2F26ac4bb55c68p1b6062jsn35368bfd8ae2",
+      "X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com",
+    },
+    body: encodedParams,
+  };
+
+  try {
+    VoiceRSS.speech({
+      key: "e44fc26cb9ed49408156cea554469cba",
+      src: joke,
+      hl: "EN-us",
+      r: 0,
+      c: "mp3",
+      f: "44khz_16bit_stereo",
+      ssml: false,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getJoke=async ()=>{
+    var response=await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single");
+    var json=await response.json();
+    joke=json.joke;
+    getVoice(joke);
+}
